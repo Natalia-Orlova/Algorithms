08bdd7a0ffa0744b982ba45bdd3b2b0476bc6562
NataliaOrlova <natakarelskaya@yandex.ru>
2023-04-06 19:34:15 +0400

    added bubblesort aeminar3

--- src/Algorithm/lesson3/seminar3.java
+++ src/Algorithm/lesson3/seminar3.java
@@ -1,28 +1,37 @@
-//package Algorithm.lesson3;
-//
-//public class seminar3 {
-//    public void sortList()
-//    {
-//        Node current = head, index = new Node();
-//        int temp;
-//
-//        if (head == null) {
-//            return;
-//        }
-//        else {
-//            while (current != null) {
-//                index = current.nextNode;
-//                while (index != null) {
-//                    if (current.value > index.value) {
-//                        temp = current.value;
-//                        current.value = index.value;
-//                        index.value = temp;
-//                    }
-//
-//                    index = index.nextNode;
-//                }
-//                current = current.nextNode;
-//            }
-//        }
-//    }
-//}
+package Algorithm.lesson3;
+
+public class seminar3 {
+    // СОРТИРОВКА СВЯЗНОГО СПИСКА
+    Node head;
+    public void sortList()
+    {
+        Node current = head, index = new Node();
+        int temp;
+
+        if (head == null) {
+            return;
+        }
+        else {
+            while (current != null) {
+                index = current.next;
+                while (index != null) {
+                    if (current.value > index.value) {
+                        temp = current.value;
+                        current.value = index.value;
+                        index.value = temp;
+                    }
+
+                    index = index.next;
+                }
+                current = current.next;
+            }
+        }
+    }
+
+    public class Node {
+        int value; //значение ноды
+        Node next; //описывает следующий элемент списка
+        Node previous; //описывает предыдущий элемент
+
+    }
+}
